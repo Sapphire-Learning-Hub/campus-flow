90018cc43a486a70f904ad8f1c8f6644842587c6
dfghj345 <franknavy@163.com>
2026-07-17 21:42:25 +0800

    style(api): 更改mock文件,使命名更规范

--- src/api/mock/browser.ts
+++ src/api/mock/browser.ts
@@ -1,4 +1,4 @@
 import { setupWorker } from "msw/browser";
-import { handlers } from "@/api/mock/handler";
+import { handlers } from "@/api/mock/handler.ts";
 
 export const worker = setupWorker(...handlers);
